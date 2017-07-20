f3d77e32dc4d266fa30f364106ce196c4dc26513
Aedan Lombardo <aedanlombardo@Aedans-MacBook-Pro.local>
2017-07-20 11:16:49 -0400

    First Synthesizer

--- src/misc/VividWork.hs
+++ src/misc/VividWork.hs
@@ -1,8 +1,29 @@
-module VividWork where
 {-# LANGUAGE DataKinds #-}
-import Vivid
 
-test :: SynthDef
-test = sd (0 ::I "note") $ do
-    s <- 0.1 ~* sinOsc (freq_ $ midiCPS (V::V "note"))
-     out 0 [s, s]
+import Vivid
+theSound = sd (60 ::I "note") $ do
+    wobble <- sinOsc (freq_ 5) ? KR ~* 10 ~+ 10
+    s <- 1 ~* sinOsc (freq_ $ midiCPS (V::V "note") ~+ wobble)
+    out 0 [s,s]
+
+loPass x = sd (60 ::I "note") $ do
+  wobble <- sinOsc (freq_ 5) ? KR ~* 10 ~+ 10
+  s <- 1 ~* sinOsc (freq_ $ midiCPS (V::V "note") ~+ wobble)
+  lo <- lpf (in_ s, freq_ x)
+  out 0 [lo,lo]
+
+main :: IO()
+main = do
+  theSound <- synth theSound ()
+  wait 2.5
+  free theSound
+  wait 2.5
+  --let synthes = map [100..200] loPass
+  --mapM synthes makeBuffer
+  loPass1 <- synth (loPass 200) ()
+  wait 2.5
+  loPass2 <- synth (loPass 60) ()
+  wait 2.5
+  free loPass1
+  free loPass2
+  return()
